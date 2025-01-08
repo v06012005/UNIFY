ba8bdeda558c46cb4f33560caab67e589daaa3a9
Tan Vinh <tanvinh06012005@gmail.com>
2025-01-08 12:54:16 +0700

    code sidebar

---
+++ frontend/app/lib/fontawesome.js
@@ -0,0 +1,3 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
+config.autoAddCss = false; // Disable auto-adding CSS
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import SideBar from "./components/SideBar";
 
 export default function Home() {
   return (
-    <div className="text-3xl">unify</div>
+    <div className="flex">
+      <SideBar></SideBar>
+      <div className="text-3xl">unify</div>
+    </div>
+
   );
 }
