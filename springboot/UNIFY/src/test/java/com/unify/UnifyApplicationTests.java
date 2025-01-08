b6503e058c4772ca2c5087469dad9b91e2cdaa1a
Tan Vinh <tanvinh06012005@gmail.com>
2025-01-08 10:26:15 +0700

    intial commit

---
+++ springboot/UNIFY/src/test/java/com/unify/UnifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
