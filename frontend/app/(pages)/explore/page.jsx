ba2d5ab226fc98e42a197ce06c36ca50e36a6ec9
Miyatoshi007 <ninhnapc09333@gmail.com>
2025-01-10 17:34:59 +0700

    add explore

--- frontend/app/(pages)/explore/page.jsx
+++ frontend/app/(pages)/explore/page.jsx
@@ -4,33 +4,83 @@ import iconImg from "@/public/imgs.svg";
 import iconReel from "@/public/vds.svg";
 const Page = () => {
   return (
-    <div className={"w-full h-auto flex flex-wrap mt-8 justify-center"}>
+    <div className={"w-full h-auto flex flex-wrap my-8 justify-center"}>
       <div className={"grid grid-cols-3 gap-2"}>
-        <div
-          className={"w-96 h-96 p-3"}
-          style={{ backgroundImage: BlackImage }}
-        >
-            <Image
-            src={iconImg} width={24} height={18} className={"float-right"}/>
+        <div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
         </div>
-        <div
-          className={"w-96 h-96"}
-          style={{ backgroundImage: BlackImage }}
-        >
-            <Image
-            src={iconReel} width={24} height={18} className={"float-right"}/>
+        <div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconReel}
+            width={24}
+            height={18}
+            alt={"iconReel"}
+            className={"float-right"}
+          />
         </div>
-        <div
-          className={"w-96 h-96"}
-          style={{ backgroundImage: BlackImage }}
-        >
-            
+        <div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
         </div>
-        <div
-          className={"w-96 h-96"}
-          style={{ backgroundImage: BlackImage }}
-        >
-            
+        <div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+        </div><div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+        </div><div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+        </div><div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+        </div><div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+        </div><div className={"w-96 h-96 bg-black p-3"}>
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
         </div>
       </div>
     </div>
