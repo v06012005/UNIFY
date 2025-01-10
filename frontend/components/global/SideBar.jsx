8821a02f9f9b9c2b1b530562b46a5d711c2336f5
Tan Vinh <tanvinh06012005@gmail.com>
2025-01-10 17:03:39 +0700

    fix sidebar and post reaction

--- frontend/components/global/SideBar.jsx
+++ frontend/components/global/SideBar.jsx
@@ -14,8 +14,8 @@ const NavButton = ({ iconClass, href = "" }) => {
 const SideBar = () => {
     return (
         <div className='flex flex-col border h-screen fixed left-0 top-0'>
-            <Image src={`/images/unify_icon_lightmode.svg`} alt='UNIFY logo' width={100} height={100} className='mx-auto'></Image>
-            <ul className='text-3xl w-full'>
+            <Image src={`/images/unify_icon_lightmode.svg`} alt='UNIFY logo' width={100} height={100} className='mx-auto flex-none h-20 w-20'></Image>
+            <ul className='text-3xl flex flex-col justify-center grow w-full'>
                 <li className='h-16'>
                     <NavButton href='/' iconClass={"fa-solid fa-house"}></NavButton>
                 </li>
@@ -26,21 +26,21 @@ const SideBar = () => {
                     <NavButton href='/' iconClass={"fa-solid fa-compass"}></NavButton>
                 </li>
                 <li className='h-16'>
-                    <NavButton iconClass={"fa-solid fa-film"}></NavButton>
+                    <NavButton href='/' iconClass={"fa-solid fa-film"}></NavButton>
                 </li>
                 <li className='h-16'>
-                    <NavButton iconClass={"fa-brands fa-facebook-messenger"}></NavButton>
+                    <NavButton href='/message' iconClass={"fa-brands fa-facebook-messenger"}></NavButton>
                 </li>
                 <li className='h-16'>
-                    <NavButton iconClass={"fa-solid fa-bell"}></NavButton>
+                    <NavButton href='/' iconClass={"fa-solid fa-bell"}></NavButton>
                 </li>
                 <li className='h-16'>
-                    <NavButton iconClass={"fa-regular fa-square-plus"}></NavButton>
-                </li>
-                <li className='h-16'>
-                    <NavButton iconClass={"fa-solid w-full fa-gear"}></NavButton>
+                    <NavButton href='/post' iconClass={"fa-regular fa-square-plus"}></NavButton>
                 </li>
             </ul>
+            <Link className='w-20 flex h-20 text-3xl items-center text-center transition delay-100 ease-in-out duration-100 hover:bg-[#D9D9D9]' href={""}>
+                <i className={`fa-solid w-full fa-gear`}></i>
+            </Link>
         </div>
     )
 }
