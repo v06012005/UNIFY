cd12c8b3301f019721082dbebe5ca520a777e8fc
Miyatoshi007 <154341775+Miyatoshi007@users.noreply.github.com>
2025-01-10 18:21:31 +0700

    Add explore (#24)
    * m
    
    * Update page.jsx
    
    * add explore (1)
    
    * Add explore (2)

---
+++ frontend/app/(pages)/explore/page.jsx
@@ -0,0 +1,258 @@
+import Image from "next/image";
+import iconImg from "@/public/imgs.svg";
+import iconReel from "@/public/vds.svg";
+import iconComment from "@/public/comment.svg";
+import iconHeart from "@/public/heart.svg";
+const Page = () => {
+  return (
+    <div className={"w-full h-auto flex flex-wrap my-8 justify-center"}>
+      <div className={"grid grid-cols-4 gap-2"}>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconReel}
+            width={24}
+            height={18}
+            alt={"iconReel"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-96 h-96 bg-black p-3 group/item hover:bg-opacity-90 cursor-pointer"
+          }
+        >
+          <Image
+            src={iconImg}
+            width={24}
+            height={18}
+            alt={"iconImg"}
+            className={"float-right"}
+          />
+          <div
+            className={
+              "h-full group/edit invisible text-white grid place-content-end float-left group-hover/item:visible"
+            }
+          >
+            <div className={"mb-2"}>
+              <Image src={iconHeart} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+            <div className={"mt-2"}>
+              <Image src={iconComment} width={20} height={20} />{" "}
+              <p className={"font-bold text-lg ml-1"}>0</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ frontend/app/(pages)/notifications/page.jsx
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div className={"h-screen w-full flex flex-col"}>
+      <div className={"grid gap-5 place-content-center"}>
+        <div className={"border-b-2 border-gray-300 w-full"}>
+            
+        </div>
+        <div className={"border-b-2 border-gray-300 w-full"}>ádasd</div>
+        <div className={"border-b-2 border-gray-300 w-full"}>ádasd</div>
+        <div className={"border-b-2 border-gray-300 w-full"}>ádasd</div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Page;
