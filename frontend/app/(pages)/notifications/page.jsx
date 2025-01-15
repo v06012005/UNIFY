49d4cef6d8875d247839878758d5edc04c8741c9
Miyatoshi007 <ninhnapc09333@gmail.com>
2025-01-15 15:13:25 +0700

    add Notifications

--- frontend/app/(pages)/notifications/page.jsx
+++ frontend/app/(pages)/notifications/page.jsx
@@ -1,29 +1,57 @@
+import Image from "next/image";
+import avatar from "@/public/images/avt.jpg";
+
 const Page = () => {
   return (
     <div className={"min-h-screen w-full flex flex-col justify-start p-10"}>
       <h1 className={"font-extrabold text-3xl font-mono mb-4"}>Notification</h1>
-      <div className={"grid gap-5 place-content-center"}>
+      <div className={"grid place-content-start"}>
         {/* Start */}
-        <div className={"p-4 bg-gray-100"}>
-          <table className={"table-fixed w-full"}>
-            <tbody>
-              <tr className={"h-16 px-2"}>
-                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                <td>Malcolm Lockyer</td>
-                <td>1961</td>
-              </tr>
-              <tr className={"h-16 px-2"}>
-                <td>Witchy Woman</td>
-                <td>The Eagles</td>
-                <td>1972</td>
-              </tr>
-              <tr className={"h-16 px-2"}>
-                <td>Shining Star</td>
-                <td>Earth, Wind, and Fire</td>
-                <td>1975</td>
-              </tr>
-            </tbody>
-          </table>
+        <div
+          className={"p-4 bg-gray-100 flex items-center justify-between"}
+          style={{ width: 50 + "em" }}
+        >
+          {/* User Info */}
+          <div className={"flex items-center gap-4"} alt="1-user">
+            <Image
+              src={avatar}
+              width={70}
+              height={70}
+              alt="User"
+              className={"rounded-full"}
+            />
+            <div className={"flex gap-3"}>
+              <p className={""}>
+                <strong className={"font-black text-xl"}>Username</strong> đã
+                follow bạn
+              </p>
+              <button
+                className={
+                  "border border-gray-300 rounded-md px-4 py-1 text-sm bg-transparent text-black"
+                }
+              >
+                Đã follow
+              </button>
+              <button
+                className={
+                  "bg-gray-500 text-white rounded-md px-4 py-1 text-sm hover:bg-gray-400"
+                }
+              >
+                Follow lại
+              </button>
+              <button
+                className={
+                  "bg-gray-500 text-white rounded-md px-4 py-1 text-sm hover:border hover:border-gray-300 hover:text-black hover:bg-transparent"
+                }
+              >
+                Follow lại
+              </button>
+            </div>
+          </div>
+          {/* Duration */}
+          <div className={"text-gray-400 text-sm"} alt="3-dur">
+            <small>30 seconds ago</small>
+          </div>
         </div>
         {/* End */}
       </div>
