441f25adecbeaa36cb98457df235a78bc05eda59
mdang-dev <minhdang25.dev@gmail.com>
2025-01-15 17:23:36 +0700

    add register page (#46)

--- frontend/api/API.js
+++ frontend/api/API.js
@@ -1,4 +1,4 @@
-export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJhODdkMzc1ZS04MWIzLTQ0ZGMtYWU0Ny00OGUzYmMxNzJkNmEiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNjc2NjUxNSwiZXhwIjoxNzM3MzcxMzE1fQ.OEoIgnOQ8IAjLCGSKvxmiCvfoZkBgsBroojWeAUszBY"
+export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJhODdkMzc1ZS04MWIzLTQ0ZGMtYWU0Ny00OGUzYmMxNzJkNmEiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczNjkxOTQ2MywiZXhwIjoxNzY4NDU1NDYzfQ.bFQM0XlARM66ahaWcMHBRpExywftSZPuY6JG19039PE"
 
 export const createMeeting = async ({ token }) => {
     const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
