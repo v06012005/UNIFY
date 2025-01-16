20952d19287d192a07ab87cd70483068c5d45edf
mdang-dev <minhdang25.dev@gmail.com>
2025-01-16 18:43:35 +0700

    add dep

--- springboot/UNIFY/src/main/java/com/unify/entity/User.java
+++ springboot/UNIFY/src/main/java/com/unify/entity/User.java
@@ -1,6 +1,49 @@
 package com.unify.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+
+@Entity
+@Table(name = "Users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "user_name")
+    String userName;
+
+    String phone;
+    String email;
+    String password;
+
+    @Column(name = "registered_at")
+    LocalDateTime registeredAt;
+
+    Boolean gender;
+    LocalDate birthDay;
+    String location;
+    String education;
+
+    @Column(name = "work_at")
+    String workAt;
 
 }
