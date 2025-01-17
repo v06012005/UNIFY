8d7026d6b5e8fe0e3763f7afeae5417fd67e1d25
v06012005 <139514810+v06012005@users.noreply.github.com>
2025-01-17 14:32:32 +0700

    Vinh (#52)
    * temp login button, updates on sidebar, add links
    
    * code settings, add layout, move edit profile

---
+++ frontend/app/(pages)/settings/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EditProfile from './edit-profile/page'
+
+const page = () => {
+    return (
+        <div>
+            <EditProfile />
+        </div>
+    )
+}
+
+export default page
