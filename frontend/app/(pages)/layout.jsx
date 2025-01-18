a5876c851911781f5f0bc428addfc93fe010c2e9
mdang-dev <minhdang25.dev@gmail.com>
2025-01-18 14:52:11 +0700

    add search

--- frontend/app/(pages)/layout.jsx
+++ frontend/app/(pages)/layout.jsx
@@ -3,9 +3,11 @@ import SideBar from "../../components/global/SideBar";
 const RootLayout = ({ children }) => {
   return (
     <>
-      <div className="flex w-full">
-        <aside className="w-20 z-50 flex-none fixed">
-              <SideBar/>
+      <div className="flex w-full ">
+        <aside className="w-20 z-50 flex-none">
+              <div className={`w-20 fixed`}>
+                  <SideBar/>
+              </div>
         </aside>
         <main className="w-full flex-initial z-10">{children}</main>
       </div>
