475a0b9e21cc027d1bda8719489ff9bd09cad47d
mdang-dev <minhdang25.dev@gmail.com>
2025-01-18 17:06:53 +0700

    Add search (#62)
    * add search

--- frontend/app/(pages)/layout.jsx
+++ frontend/app/(pages)/layout.jsx
@@ -3,11 +3,13 @@ import SideBar from "../../components/global/SideBar";
 const RootLayout = ({ children }) => {
   return (
     <>
-      <div className="flex w-full">
-        <aside className="w-20 flex-none">
-          <SideBar />
+      <div className="flex w-full ">
+        <aside className="w-20 z-50 flex-none">
+              <div className={`w-20 fixed`}>
+                  <SideBar/>
+              </div>
         </aside>
-        <main className="w-full flex-initial">{children}</main>
+        <main className="w-full flex-initial z-10">{children}</main>
       </div>
     </>
   );
---
+++ frontend/components/global/UserHistorySearch.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import {X} from "lucide-react";
+
+const UserHistorySearch = ({avatar, username, profile, followers}) => {
+    return (
+        <div className={`w-full h-12 flex items-center gap-3`}>
+            <Image src={avatar} alt={"img"} width={40} height={40} className={`rounded-full`}/>
+            <div className={`grid`}>
+                <span className={`text-sm font-bold`}>{username}</span>
+                <span className={`text-sm`}>{profile}</span>
+                <span className={`text-[12px] text-gray-900-`}>{followers} followers</span>
+            </div>
+            <div className={`ml-auto cursor-pointer`}>
+             <X/>
+            </div>
+        </div>
+    )
+}
+
+export default UserHistorySearch;
