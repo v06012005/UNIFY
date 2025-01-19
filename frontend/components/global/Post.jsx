1f78e90eadf669c6b5e34251cffd0e47edbbaf32
Tan Vinh <tanvinh06012005@gmail.com>
2025-01-19 11:41:20 +0700

    add hero ui

--- frontend/components/global/Post.jsx
+++ frontend/components/global/Post.jsx
@@ -4,9 +4,12 @@ import React from 'react'
 import Image from 'next/image'
 import dummy from '@/public/images/dummy.png'
 import avatar from '@/public/images/test1.png'
-import PostReaction from './PostReaction'
 import Link from 'next/link'
 import { useState } from 'react'
+import LikeButton from './LikeButton';
+import CommentButton from './CommentButton';
+import ShareButton from './ShareButton';
+import TextArea from './TextArea';
 
 const User = () => {
     return (
@@ -39,11 +42,15 @@ const Caption = ({ text, maxLength = 100 }) => {
 
 const Post = ({ imageSrc = dummy }) => {
     return (
-        <div className='aspect-video w-3/4 mb-8 mx-auto pb-8 border-b border-black'>
+        <div className='aspect-video w-3/4 mb-8 mx-auto pb-8'>
             <User></User>
             <Image src={imageSrc} alt='Dummy' className='w-full max-h-svh mb-2 object-cover mx-auto rounded-lg' width={500} height={400} />
             <Caption text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quibusdam, ex maiores amet alias dolor minima magnam quis totam molestias consectetur laudantium possimus et asperiores? Dignissimos minima animi omnis sed! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, id repellat minus labore esse eligendi maiores asperiores? Architecto dolorem veritatis, totam nam, molestiae quo quis asperiores qui nostrum animi possimus?`} />
-            <PostReaction></PostReaction>
+            <div className='flex text-xl'>
+                <LikeButton />
+                <CommentButton />
+                <ShareButton />
+            </div>
             <div className='mt-2 flex flex-wrap'>
                 <Hashtag content='#myhashtag'></Hashtag>
                 <Hashtag content='#myhashtag'></Hashtag>
@@ -62,9 +69,10 @@ const Post = ({ imageSrc = dummy }) => {
             </div>
             <form action="#">
                 <div className="mt-2">
-                    <div className="flex items-center rounded-md bg-white pl-3 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2">
+                    {/* <div className="flex items-center rounded-md bg-white pl-3 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2">
                         <input type="text" name="comment" id="comment" className="block text-wrap min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-90 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="Add a comment" />
-                    </div>
+                    </div> */}
+                    <TextArea />
                 </div>
             </form>
         </div>
