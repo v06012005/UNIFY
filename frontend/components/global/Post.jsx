ea1696d0b385f209df0b50596e217cc57c1349a9
kdksakdaskdk <tri00349@gmail.com>
2025-01-20 16:03:03 +0700

    adminPost (#73)

--- frontend/components/global/Post.jsx
+++ frontend/components/global/Post.jsx
@@ -1,5 +1,27 @@
 "use client";
 
+<<<<<<< HEAD
+import React from "react";
+import Image from "next/image";
+import dummy from "@/public/images/dummy.png";
+import avatar from "@/public/images/test1.png";
+import Link from "next/link";
+import { useState } from "react";
+import LikeButton from "./LikeButton";
+import CommentButton from "./CommentButton";
+import ShareButton from "./ShareButton";
+import TextArea from "./TextArea";
+import { Textarea } from "@heroui/react";
+import CommentForm from "./CommentForm";
+
+const User = () => {
+  return (
+    <div className="flex mb-2">
+      <Image src={avatar} alt="Avatar" className="rounded-full w-14 h-14" />
+      <p className="my-auto ml-3 text-lg font-bold">@username</p>
+    </div>
+  );
+=======
 import React from 'react'
 import Image from 'next/image'
 import dummy from '@/public/images/dummy.png'
@@ -26,9 +48,11 @@ const User = ({ href = "" }) => {
             </div>
         </Link>
     );
+>>>>>>> ed5ed417dca6c05a1f39cc277fd1597901fef8d9
 };
 
 const Hashtag = ({ content = "", to = "" }) => {
+<<<<<<< HEAD
     return (
         <Link
             href={to}
@@ -37,11 +61,80 @@ const Hashtag = ({ content = "", to = "" }) => {
             {content}
         </Link>
     );
+=======
+  return (
+    <Link
+      href={to}
+      className="text-lg text-sky-500 mr-4 hover:underline hover:decoration-sky-500"
+    >
+      {content}
+    </Link>
+  );
+>>>>>>> 4b0ba80 (adminPost (#73))
 };
 
 const Caption = ({ text, maxLength = 100 }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
+<<<<<<< HEAD
+  const toggleExpanded = () => setIsExpanded(!isExpanded);
+  return (
+    <>
+      <div className="mb-2 leading-snug text-wrap">
+        {isExpanded ? text : `${text.slice(0, maxLength)}...`}
+        <button
+          onClick={toggleExpanded}
+          className="text-gray-500 font-semibold ml-2"
+        >
+          {isExpanded ? "Less" : "More"}
+        </button>
+      </div>
+    </>
+  );
+};
+
+const Post = ({ imageSrc = dummy }) => {
+  return (
+    <div className="aspect-video w-3/4 mb-8 mx-auto pb-8">
+      <User></User>
+      <Image
+        src={imageSrc}
+        alt="Dummy"
+        className="w-full max-h-svh mb-2 object-cover mx-auto rounded-lg"
+        width={500}
+        height={400}
+      />
+      <Caption
+        text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quibusdam, ex maiores amet alias dolor minima magnam quis totam molestias consectetur laudantium possimus et asperiores? Dignissimos minima animi omnis sed! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, id repellat minus labore esse eligendi maiores asperiores? Architecto dolorem veritatis, totam nam, molestiae quo quis asperiores qui nostrum animi possimus?`}
+      />
+      <div className="flex text-xl">
+        <LikeButton className="!text-xl" />
+        <CommentButton />
+        <ShareButton />
+      </div>
+      <div className="mt-2 flex flex-wrap">
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+        <Hashtag content="#myhashtag"></Hashtag>
+      </div>
+      <div className="mt-2">
+        <button className="text-black hover:text-gray-500 text-md dark:text-gray-400 dark:hover:text-white">
+          View all comments
+        </button>
+      </div>
+      <CommentForm />
+    </div>
+  );
+};
+=======
     const toggleExpanded = () => setIsExpanded(!isExpanded);
     return (
         <>
@@ -126,5 +219,6 @@ const Post = ({ srcs }) => {
         </div>
     )
 }
+>>>>>>> ed5ed417dca6c05a1f39cc277fd1597901fef8d9
 
 export default Post;
