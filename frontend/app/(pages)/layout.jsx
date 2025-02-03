98480a61b3b124be8f4f906c264a2b943de43b0f
mdang-dev <minhdang25.dev@gmail.com>
2025-02-03 21:48:07 +0700

    write api

--- frontend/app/(pages)/layout.jsx
+++ frontend/app/(pages)/layout.jsx
@@ -7,7 +7,6 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 const RootLayout = ({ children }) => {
   return (
     <>
-<<<<<<< HEAD
       <HeroUIProvider className="w-full">
         <NextThemesProvider attribute="class" defaultTheme="dark">
           <div className="flex w-full light text-foreground bg-background dark:bg-black">
@@ -20,14 +19,6 @@ const RootLayout = ({ children }) => {
           </div>
         </NextThemesProvider>
       </HeroUIProvider>
-=======
-      <div className="flex w-full">
-        <aside className="w-20 z-50 flex-none fixed">
-              <SideBar/>
-        </aside>
-        <main className="w-full flex-initial z-10">{children}</main>
-      </div>
->>>>>>> e240531 (add search)
     </>
   );
 };
--- frontend/components/global/TextSearchHistory.jsx
+++ frontend/components/global/TextSearchHistory.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { UserSearch, X } from "lucide-react";
 
 const TextSearchHistory = ({ text }) => {
@@ -16,22 +15,3 @@ const TextSearchHistory = ({ text }) => {
 };
 
 export default TextSearchHistory;
-=======
-import {UserSearch, X} from "lucide-react";
-
-const TextSearchHistory = ({text}) => {
-    return (
-        <div className={`w-full h-12 flex items-center gap-3`}>
-            <UserSearch size={40}/>
-            <div className={`grid`}>
-                <span className={`text-md`}>{text}</span>
-            </div>
-            <div className={`ml-auto cursor-pointer`}>
-                <X/>
-            </div>
-        </div>
-    )
-}
-
-export default TextSearchHistory;
->>>>>>> e240531 (add search)
