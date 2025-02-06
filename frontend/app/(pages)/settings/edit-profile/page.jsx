34ef4157bb9a674beb938cb0c42584d9de4df34a
Huynh Vy <huynhthithaovy.hg.2021@gmail.com>
2025-02-06 21:52:59 +0700

    codeEditProfile

--- frontend/app/(pages)/settings/edit-profile/page.jsx
+++ frontend/app/(pages)/settings/edit-profile/page.jsx
@@ -4,6 +4,10 @@ import React from "react";
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
 const NavButton = ({ iconClass, href = "", content = "" }) => {
   return (
     <Link className="flex h-full items-center text-center" href={href}>
@@ -12,37 +16,114 @@ const NavButton = ({ iconClass, href = "", content = "" }) => {
     </Link>
   );
 };
+
+const validationSchema = Yup.object({
+  biography: Yup.string().max(500, 'Biography should be less than 500 characters').optional(),
+  firstName: Yup.string().required('First name is required'),
+  lastName: Yup.string().required('Last name is required'),
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  phone: Yup.string().matches(
+    /^[0-9]{10}$/,
+    'Phone number should be 10 digits'
+  ).required('Phone number is required'),
+  gender: Yup.string().required('Gender is required'),
+  birthday: Yup.object({
+    day: Yup.number().min(1, 'Invalid day').max(31, 'Invalid day').required('Day is required'),
+    month: Yup.number().min(1, 'Invalid month').max(12, 'Invalid month').required('Month is required'),
+    year: Yup.number().min(1900, 'Invalid year').max(2100, 'Invalid year').required('Year is required'),
+  }),
+  location: Yup.string().required('Location is required'),
+  educationWork: Yup.string().required('Education/Work is required'),
+  avatar: Yup.mixed().required('Avatar is required'),
+});
+
 const Page = () => {
+  
+  const defaultAvatar = "/images/avt.jpg"; 
+  const [avatar, setAvatar] = useState(defaultAvatar);
+
+  const handleChangeAvatar = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setAvatar(reader.result); 
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDeleteAvatar = () => {
+    setAvatar(defaultAvatar); 
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      biography: "",
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      phone: "",
+      gender: "",
+      birthday: { day: "", month: "", year: "" },
+      location: "",
+      educationWork: "",
+      avatar: null,
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+
   return (
     <div className="w-full">
       <div className="h-screen overflow-y-auto">
-        <form>
-          <div className="flex m-5 bg-gray-200 dark:bg-gray-800 rounded-xl items-center pr-5">
-            <div className="flex-shrink-0 p-2">
-              <Image
-                src={`/images/avt.jpg`}
-                alt="Avatar"
-                className="rounded-full border-2 border-gray-300"
-                width={100}
-                height={100}
-              />
-            </div>
-            <div className="ml-4">
-              <p className="text-2xl">huynhdiz</p>
-              <p className="font-bold">Huỳnh Thị Thảo Vy</p>
-            </div>
-            <div className="flex-grow flex justify-end gap-4">
-              <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition">
-                Change Avatar
-              </button>
-              <button className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition">
-                Delete Avatar
-              </button>
-              <button className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition">
-                Logout
-              </button>
-            </div>
-          </div>
+        <form onSubmit={formik.handleSubmit}>
+        <div className="flex m-5 bg-gray-200 dark:bg-gray-800 rounded-xl items-center pr-5">
+      <div className="flex-shrink-0 p-2">
+        <div className="w-[100px] h-[100px] rounded-full border-2 border-gray-300 overflow-hidden">
+          <Image
+            src={avatar}
+            alt="Avatar"
+            width={100}
+            height={100}
+            className="object-cover w-full h-full"
+          />
+        </div>
+      </div>
+      <div className="ml-4">
+        <p className="text-2xl">huynhdiz</p>
+        <p className="font-bold">Huỳnh Thị Thảo Vy</p>
+      </div>
+      <div className="flex-grow flex justify-end gap-4">
+
+        <label htmlFor="avatar-upload" className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition cursor-pointer">
+          Change Avatar
+          <input
+            id="avatar-upload"
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={handleChangeAvatar}
+          />
+        </label>
+
+        <button
+          className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition"
+          onClick={handleDeleteAvatar}
+        >
+          Delete Avatar
+        </button>
+         
+        <button className="bg-gray-500 text-white py-2 px-4 rounded-lg hover:bg-gray-600 transition">
+          Logout
+        </button>
+      </div>
+    </div>
 
           <div className="m-5">
             <label
@@ -55,8 +136,12 @@ const Page = () => {
               id="input-field"
               type="text"
               placeholder="Enter your biography"
+              {...formik.getFieldProps("biography")}
               className="w-full px-4 py-2 border border-gray-300 dark:bg-black dark:border-gray-600 rounded-lg focus:ring-1 focus:ring-gray-500 focus:outline-none hover:border-gray-500 hover:shadow-md transition"
             />
+             {formik.touched.biography && formik.errors.biography && (
+              <div className="text-red-500 text-sm">{formik.errors.biography}</div>
+            )}
           </div>
 
           <div className="m-5 flex gap-4">
@@ -71,8 +156,12 @@ const Page = () => {
                 id="first-name"
                 type="text"
                 placeholder="Enter your first name"
+              {...formik.getFieldProps("first-name")}
                 className="w-full px-4 py-2 border border-gray-300 dark:bg-black dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-gray-500 focus:outline-none hover:border-gray-500"
               />
+              {formik.touched.firstName && formik.errors.firstName && (
+                <div className="text-red-500 text-sm">{formik.errors.firstName}</div>
+              )}
             </div>
 
             <div className="flex-1">
@@ -86,8 +175,12 @@ const Page = () => {
                 id="last-name"
                 type="text"
                 placeholder="Enter your last name"
+                {...formik.getFieldProps("lastName")}
                 className="w-full px-4 py-2 border border-gray-300 dark:bg-black dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-gray-500 focus:outline-none hover:border-gray-500"
               />
+               {formik.touched.lastName && formik.errors.lastName && (
+                <div className="text-red-500 text-sm">{formik.errors.lastName}</div>
+              )}
             </div>
 
             <div className="flex-1">
@@ -101,8 +194,12 @@ const Page = () => {
                 id="username"
                 type="text"
                 placeholder="Enter your username"
+                {...formik.getFieldProps("username")}
                 className="w-full px-4 py-2 border border-gray-300 dark:bg-black dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-gray-500 focus:outline-none hover:border-gray-500"
               />
+               {formik.touched.username && formik.errors.username && (
+              <div className="text-red-500 text-sm">{formik.errors.username}</div>
+            )}
             </div>
           </div>
 
@@ -116,10 +213,14 @@ const Page = () => {
               </label>
               <input
                 id="email"
-                type="email"
+                type="text"
                 placeholder="Enter your email"
+                {...formik.getFieldProps("email")}
                 className="w-full px-4 py-2 border border-gray-300 dark:bg-black dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-gray-500 focus:outline-none hover:border-gray-500"
               />
+               {formik.touched.email && formik.errors.email && (
+              <div className="text-red-500 text-sm">{formik.errors.email}</div>
+            )}
             </div>
 
             <div className="flex-1">
@@ -228,7 +329,7 @@ const Page = () => {
           </div>
 
           <div className="m-5 flex justify-end">
-            <button className="px-10 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 dark:bg-gray-500 dark:hover:bg-gray-600 transition focus:outline-none focus:ring-2 focus:ring-blue-500 text-xl">
+            <button type="submit" className="px-10 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 dark:bg-gray-500 dark:hover:bg-gray-600 transition focus:outline-none focus:ring-2 focus:ring-blue-500 text-xl">
               Save
             </button>
           </div>
