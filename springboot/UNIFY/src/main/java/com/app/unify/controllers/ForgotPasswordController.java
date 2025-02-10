bf38f21b5c9bf5761ec4f697ff05b1d17472cf64
Miyatoshi007 <154341775+Miyatoshi007@users.noreply.github.com>
2025-02-10 23:23:08 +0700

    update forgotpass (#93)

--- springboot/UNIFY/src/main/java/com/app/unify/controllers/ForgotPasswordController.java
+++ springboot/UNIFY/src/main/java/com/app/unify/controllers/ForgotPasswordController.java
@@ -1,11 +1,12 @@
-package com.app.unify.controller;
+package com.app.unify.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.unify.service.SpringMailService;
+import com.app.unify.services.SpringMailService;
+import com.app.unify.utils.OtpGeneratorUtil;
 
 @RestController
 public class ForgotPasswordController {
@@ -13,9 +14,9 @@ public class ForgotPasswordController {
 	private SpringMailService mailService;
 
 	@GetMapping("/send-email")
-	public String sendMail(@RequestParam("to") String to, @RequestParam("subject") String subject
-			) {
-		mailService.sendOtpCode(to, subject,null);
-		return "Đã gửi OTP";
+	public String sendMail(@RequestParam("to") String to) {
+		String otp = OtpGeneratorUtil.generatorOTP();
+		mailService.sendOtpMail(to, otp);
+		return "Đã gửi OTP tới " + to;
 	}
 }
