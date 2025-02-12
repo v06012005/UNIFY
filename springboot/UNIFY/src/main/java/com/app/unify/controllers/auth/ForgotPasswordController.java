a655bba39e2a9e2d25c7e6e3635dd76be0ec10df
mdang-dev <minhdang25.dev@gmail.com>
2025-02-12 20:03:12 +0700

    Add chat app (#96)
    * config chat app
    
    * config mongodb
    
    * config mongodb
    
    * config postgres with cloud
    
    * fix conflict
    
    * modified file name

--- springboot/UNIFY/src/main/java/com/app/unify/controllers/auth/ForgotPasswordController.java
+++ springboot/UNIFY/src/main/java/com/app/unify/controllers/auth/ForgotPasswordController.java
@@ -1,4 +1,4 @@
-package com.app.unify.controllers;
+package com.app.unify.controllers.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ springboot/UNIFY/src/main/java/com/app/unify/entities/ChatMessage.java
@@ -0,0 +1,23 @@
+package com.app.unify.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    @Id
+    ObjectId id;
+    String content;
+    String sender;
+
+}
