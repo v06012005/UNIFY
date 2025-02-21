2e4ef8b396fdf7f6946f6b21aa1be91e07afe3be
mdang-dev <minhdang25.dev@gmail.com>
2025-02-21 22:26:48 +0700

    make video calling

--- springboot/UNIFY/src/main/java/com/app/unify/dto/global/SignalData.java
+++ springboot/UNIFY/src/main/java/com/app/unify/dto/global/SignalData.java
@@ -1,4 +1,10 @@
 package com.app.unify.dto.global;
+import lombok.Data;
 
+@Data
 public class SignalData {
+    private String sdp;
+    private String type;
 }
+
+
--- springboot/UNIFY/src/main/java/com/app/unify/dto/global/SignalMessageDTO.java
+++ springboot/UNIFY/src/main/java/com/app/unify/dto/global/SignalMessageDTO.java
@@ -1,21 +1,20 @@
 package com.app.unify.dto.global;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class SignalMessage {
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SignalMessageDTO {
 
-    private String type;
-    private String signalData;
-    private String from;
-    private String name;
-    private String userToCall;
-    private String to;
+    String type;
+    SignalData signalData;
+    String from;
+    String name;
+    String userToCall;
+    String to;
 
 }
