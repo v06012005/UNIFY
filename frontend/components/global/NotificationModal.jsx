eac33cfb8bc44d7606bc9dae41a60145b2015356
Miyatoshi007 <154341775+Miyatoshi007@users.noreply.github.com>
2025-03-03 05:08:08 +0700

    commit (#150)

--- frontend/components/global/NotificationModal.jsx
+++ frontend/components/global/NotificationModal.jsx
@@ -3,33 +3,28 @@
 import React from "react";
 import { FollowNotification } from "@/components/ui/follow_notification.jsx";
 import { TagNotification } from "@/components/ui/tag_notification";
+import useNotification from "@/hooks/useNotification";
 
-const NotificationModal = ({ isNotificationOpen, modalRef }) => {
-  const notifications = Array.from({ length: 9 }, (_, index) => ({
-    id: index + 1,
-    type: index % 2 === 0 ? "follow" : "tag",
-    isSeen: index > 3,
-    timestamp: Date.now() - index * 1000 * 60 * 5,
-  }));
+const NotificationModal = ({ isNotificationOpen, modalRef, userId }) => {
+  const { notifications } = useNotification(userId);
 
-  // Sắp xếp thông báo theo thời gian
-  const sortedNotifications = notifications.sort(
-    (a, b) => b.timestamp - a.timestamp
+  const sortedNotifications = [...notifications].sort(
+    (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
   );
 
-  // Render notifications
   const renderNotification = (notification) => {
-    switch (notification.type) {
+    switch (notification.type.toLowerCase()) {
       case "follow":
         return (
           <FollowNotification
             key={notification.id}
-            isSeen={notification.isSeen}
+            isSeen={notification.isRead}
+            sender={notification.sender} // Pass the sender prop
           />
         );
       case "tag":
         return (
-          <TagNotification key={notification.id} isSeen={notification.isSeen} />
+          <TagNotification key={notification.id} isSeen={notification.isRead} />
         );
       default:
         return null;
@@ -41,9 +36,7 @@ const NotificationModal = ({ isNotificationOpen, modalRef }) => {
       <div
         ref={modalRef}
         className={`bg-white dark:bg-black text-black dark:text-white shadow-lg max-w-md h-screen overflow-hidden ${
-          isNotificationOpen
-            ? "animate-fadeScale"
-            : "animate-fadeOut"
+          isNotificationOpen ? "animate-fadeScale" : "animate-fadeOut"
         } transition-all ease-in-out duration-300`}
         style={{
           width: isNotificationOpen ? 448 : 0,
@@ -52,14 +45,20 @@ const NotificationModal = ({ isNotificationOpen, modalRef }) => {
         <h1 className="font-bold text-2xl mb-4 px-5 pt-5">Notifications</h1>
 
         <div className="overflow-y-auto space-y-1 pr-2 max-h-full h-[94%] pb-5 pl-5">
-          {sortedNotifications.map((notification, index) => (
-            <React.Fragment key={notification.id}>
-              {renderNotification(notification)}
-              {index < sortedNotifications.length - 1 && (
-                <hr className="border-t border-gray-300 dark:border-gray-600" />
-              )}
-            </React.Fragment>
-          ))}
+          {sortedNotifications.length > 0 ? (
+            sortedNotifications.map((notification, index) => (
+              <React.Fragment key={notification.id}>
+                {renderNotification(notification)}
+                {index < sortedNotifications.length - 1 && (
+                  <hr className="border-t border-gray-300 dark:border-gray-600" />
+                )}
+              </React.Fragment>
+            ))
+          ) : (
+            <p className="text-center text-gray-400 dark:text-gray-600 mt-10">
+              No notifications yet
+            </p>
+          )}
         </div>
       </div>
     </div>
--- frontend/components/ui/follow_notification.jsx
+++ frontend/components/ui/follow_notification.jsx
@@ -2,39 +2,44 @@
 
 import React from "react";
 import Image from "next/image";
-import Avatar from "@/public/images/avt.jpg";
 import FollowButton from "./follow-button";
+import Avatar from "@/public/images/avt.jpg";
+import { useApp } from "../provider/AppProvider";
 
-export const FollowNotification = React.memo(
-  ({ isSeen = false }) => {
-    return (
-      <div
-        className={`p-2 px-4 rounded-lg items-center max-h-[88px] ${
-          isSeen ? "" : "bg-gray-100 dark:bg-gray-800"
-        }`}
-      >
-        <div className="flex items-center gap-4">
-          <Image
-            src={Avatar}
-            alt="User"
-            className="rounded-full w-[64px] h-[64px]"
-          />
-          <div className={"flex flex-col"}>
-            <div className={"flex gap-2"}>
-              <p>
-                <strong className={"font-extrabold text-lg"}>Username</strong>{" "}
-                has started following you.
-              </p>
-              <FollowButton
-                followingId={"80d94ed8-29a0-4940-aef3-837518ec2cf8"}
-                classFollow="bg-red-500 text-white rounded-md px-2 py-1 w-[40%] h-[100%] self-center"
-                classFollowing="bg-transparent text-white border border-gray-300 rounded-md px-2 py-1 text-sm bg-transparent w-[40%] h-[100%] self-center"
-              />
-            </div>
-            <small className={"text-gray-400 text-sm"}>30 seconds ago</small>
+export const FollowNotification = React.memo(({ isSeen = false, sender }) => {
+  const { user } = useApp();
+  return (
+    <div
+      className={`p-2 px-4 rounded-lg items-center max-h-[88px] ${
+        isSeen ? "" : "bg-gray-100 dark:bg-gray-800"
+      }`}
+    >
+      <div className="flex items-center gap-4">
+        <Image
+          src={sender.avatar || Avatar}
+          alt="User"
+          className="rounded-full w-[64px] h-[64px]"
+        />
+        <div className={"flex flex-col"}>
+          <div className={"flex gap-2"}>
+            <p>
+              <strong className={"font-extrabold text-lg"}>
+                {sender.username}
+              </strong>{" "}
+              has started following you.
+            </p>
+            <FollowButton
+              userId={user.id}
+              followingId={sender.id}
+              classFollow="bg-red-500 text-white rounded-md px-2 py-1 w-[40%] h-[100%] self-center"
+              classFollowing="bg-transparent text-white border border-gray-300 rounded-md px-2 py-1 text-sm bg-transparent w-[40%] h-[100%] self-center"
+              contentFollow="Follow"
+              contentFollowing="Following"
+            />
           </div>
+          <small className={"text-gray-400 text-sm"}>30 seconds ago</small>
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
